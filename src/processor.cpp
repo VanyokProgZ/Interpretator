6cda6d17ce4a0868f57fde7e6e73e5690c8a5187
VanyokProgZ <ivangzolkin@gmail.com>
2025-05-29 02:31:10 +0300

    try fix cmake

--- src/processor.cpp
+++ src/processor.cpp
@@ -5,58 +5,77 @@
 #include <conio.h>
 #define CLS "cls"
 #else
-#include <curses.h>
+#include <ncurses.h>
 #define CLS "clear"
 #endif
 
 Processor::Processor(const std::string& input_file)
 {
-	call_stack.push(MemoryBlock(Lexem("NULL POINTER",NULL_POINER_CALL_STACK_BOTTOM,NULL_POINER_CALL_STACK_BOTTOM,0,"STACK BOTTOM","STACK BOTTOM"), Lexem("NULL POINTER", NULL_POINER_CALL_STACK_BOTTOM, NULL_POINER_CALL_STACK_BOTTOM, 0, "STACK BOTTOM", "STACK BOTTOM"), Lexem("NULL POINTER", NULL_POINER_CALL_STACK_BOTTOM, NULL_POINER_CALL_STACK_BOTTOM, 0, "STACK BOTTOM", "STACK BOTTOM"),0));
-	GLOBAL_SECTOR_LAST = 0;
-	GLOBAL_SCAN = false;
-	from_file_to_code_segment(input_file, code_segment);
+    call_stack.push(MemoryBlock(Lexem("NULL POINTER", NULL_POINER_CALL_STACK_BOTTOM, NULL_POINER_CALL_STACK_BOTTOM, 0, "STACK BOTTOM", "STACK BOTTOM"), Lexem("NULL POINTER", NULL_POINER_CALL_STACK_BOTTOM, NULL_POINER_CALL_STACK_BOTTOM, 0, "STACK BOTTOM", "STACK BOTTOM"), Lexem("NULL POINTER", NULL_POINER_CALL_STACK_BOTTOM, NULL_POINER_CALL_STACK_BOTTOM, 0, "STACK BOTTOM", "STACK BOTTOM"), 0));
+    GLOBAL_SECTOR_LAST = 0;
+    GLOBAL_SCAN = false;
+    from_file_to_code_segment(input_file, code_segment);
 }
+
 void Processor::run() {
-
-	std::string current_row;
-	Lexem_Queue_Assembler queue_assembler(this);
-	code_segment.insert_input_point();
-	while (!code_segment.isEmpty()) {
-
-		if (__DEBUG__STATE__) {
-			code_segment.print_all();
-			std::cout << "\n\n-------------------------------------------------------\n1 - print call_stack\nsth else - next row\n\n";
 #ifdef _WIN32
-			char key = _getch();
 #else
-			char key = getch();
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
 #endif
-			if (key == '1') {
-				call_stack.print_all();
-				std::cout << "\n";
+
+    std::string current_row;
+    Lexem_Queue_Assembler queue_assembler(this);
+    code_segment.insert_input_point();
+
+    while (!code_segment.isEmpty()) {
+        if (__DEBUG__STATE__) {
+            code_segment.print_all();
+            std::cout << "\n\n-------------------------------------------------------\n1 - print call_stack\nsth else - next row\n\n";
+
 #ifdef _WIN32
-				key = _getch();
+            char key = _getch();
 #else
-				key = getch();
+            char key = wgetch(stdscr);
 #endif
-			}
 
-			system(CLS);
-		}
-		current_row = code_segment.get_next_row();
-		if (current_row == code_segment.true_main) {
-			GLOBAL_SCAN = true;
-		}
-		cut_white_begin(current_row);
-		past_spaces(current_row);
+            if (key == '1') {
+                call_stack.print_all();
+                std::cout << "\n";
 
-		if (code_segment.isEmpty()) GLOBAL_SCAN = true;
+#ifdef _WIN32
+                key = _getch();
+#else
+                key = wgetch(stdscr);
+#endif
+            }
 
-		auto lexems_row = lexemize_row(current_row, Class_names_list);
+            int result = system(CLS);
+            if (result != 0) {
+                std::cerr << "Error executing command: " << CLS << std::endl;
+            }
+        }
 
-		for (size_t i = 0; i < lexems_row.size(); i++) {
-			queue_assembler.push_back(lexems_row[i]);
-		}
-	}
-	
+        current_row = code_segment.get_next_row();
+        if (current_row == code_segment.true_main) {
+            GLOBAL_SCAN = true;
+        }
+        cut_white_begin(current_row);
+        past_spaces(current_row);
+
+        if (code_segment.isEmpty()) GLOBAL_SCAN = true;
+
+        auto lexems_row = lexemize_row(current_row, Class_names_list);
+
+        for (size_t i = 0; i < lexems_row.size(); i++) {
+            queue_assembler.push_back(lexems_row[i]);
+        }
+    }
+
+#ifdef _WIN32
+#else
+    endwin();
+#endif
 }
